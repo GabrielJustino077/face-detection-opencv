e34cc034d7b192daee306a3f4511ec15cbf5bae7
GabrielJustino077 <gjgs@cin.ufpe.br>
2026-04-05 01:59:31 -0300

    Update detector.py

--- detector.py
+++ detector.py
@@ -1,25 +1,25 @@
 import cv2
 
 # carregar classificador de rosto
-rosto_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+classificador = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # abrir webcam
-cap = cv2.VideoCapture(0)
+webcam = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
+    sucesso, imagem = webcam.read()
 
-    cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
 
-    rostos = rosto_cascade.detectMultiScale(cinza, 1.3, 5)
+    rostos = classificador.detectMultiScale(cinza, 1.3, 5)
 
     for (x, y, w, h) in rostos:
-        cv2.rectangle(frame, (x,y), (x+w,y+h), (255,0,0), 2)
+        cv2.rectangle(imagem, (x,y), (x+w,y+h), (255,0,0), 2)
 
-    cv2.imshow('Detector de Rosto', frame)
+    cv2.imshow('Detector de Rosto', imagem)
 
     if cv2.waitKey(1) == 27:
         break
 
-cap.release()
+webcam.release()
 cv2.destroyAllWindows()
